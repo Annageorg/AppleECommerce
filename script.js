20343a7d8696a0f25b275581fa06792ab8650b0d
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-06-10 19:00:51 +0100

    section 3 open lap top transitions bug fixed

--- script.js
+++ script.js
@@ -79,3 +79,15 @@ document.querySelector('.bottom-z-control').addEventListener('click', () => {
 })
 playPause()
 // End of Cuboid controls
+
+
+// Section 3
+const section3Content = document.querySelector('.section-3-content')
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset + window.innerHeight >= 
+        section3Content.offsetTop + section3Content.offsetHeight /2) {
+          section3Content.classList.add('change')
+    }
+})
+// End of Section 3
